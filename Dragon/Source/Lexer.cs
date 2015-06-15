8d14624644597ef8c9f7eb6c2b73531ce601fc04
moophy <MoophyAlan@gmail.com>
2015-06-16 03:07:53 +1200

    new file:   Dragon/Source/Lexer.cs

---
+++ Dragon/Source/Lexer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source
+{
+    public class Tag
+    {
+        public readonly static int
+            AND     =   256,
+            BASIC   =   257,
+            BREAK   =   258,
+            DO      =   259,
+            ELSE    =   260,
+            EQ      =   261,
+            FALSE   =   262,
+            GE      =   263,
+            ID      =   264,
+            IF      =   265,
+            INDEX   =   266,
+            LE      =   267,
+            MINUS   =   268,
+            NE      =   269,
+            NUM     =   270,
+            OR      =   271,
+            REAL    =   272,
+            TEMP    =   273,
+            TRUE    =   274,
+            WHILE   =   275;
+    }
+
+    class Lexer
+    {
+    }
+}
