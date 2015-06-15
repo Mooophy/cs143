d9321375f07170cfb16c300df8fab4cfd4070d3a
moophy <MoophyAlan@gmail.com>
2015-06-16 11:40:49 +1200

    modified:   Symbols.cs

--- Dragon/Source/Symbols.cs
+++ Dragon/Source/Symbols.cs
@@ -14,6 +14,7 @@ namespace Dragon
             Int     =   new Type("int",     Tag.BASIC, 4),
             Float   =   new Type("float",   Tag.BASIC, 8),
             Char    =   new Type("char",    Tag.BASIC, 1),
-            Bool    =   new Type("bool",    Tag.BASIC, 1);   
+            Bool    =   new Type("bool",    Tag.BASIC, 1);
+        public static bool Numeric(Type type) { return type == Type.Char || type == Type.Int || type == Type.Float; }
     }
 }
