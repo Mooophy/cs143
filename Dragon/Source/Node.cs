c4c452f2a02d08de38c8f519f0419ea1b5fa5990
moophy <MoophyAlan@gmail.com>
2015-06-18 03:02:35 +1200

    add 	new file:   Dragon/Source/Node.cs

---
+++ Dragon/Source/Node.cs
@@ -0,0 +1,42 @@
+﻿//
+//  corresponds to Inter package in dragon book
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dragon
+{
+    public class Node
+    {
+        int _lexLine;
+        static int _labels = 0;
+        Node() 
+        { 
+            _lexLine = Lexer.Line; 
+        }
+
+        void Error(string msg)
+        {
+            throw new Exception(msg);
+        }
+
+        public int NewLable()
+        {
+            return ++Node._labels;
+        }
+
+        public void EmitLabel(int i)
+        {
+            Console.Write("L" + i + ":");
+        }
+
+        public void Emit(string s)
+        {
+            Console.Write("\t" + s);
+        }
+    }
+}
