04d9bc7d0c993b662655ec932627a9fde4f9596f
moophy <MoophyAlan@gmail.com>
2015-06-18 13:53:28 +1200

    add Expr 	modified:   Dragon/Source/Node.cs

--- Dragon/Source/Node.cs
+++ Dragon/Source/Node.cs
@@ -40,4 +40,57 @@ namespace Dragon
             Console.Write("\t" + s);
         }
     }
+
+
+    public class Expr : Node
+    {
+        public Token Op { get; set; }
+        public Type Type { get; set; }
+
+        public Expr(Token tok, Type type)
+        {
+            this.Op = tok;
+            this.Type = type;
+        }
+
+        public Expr Gen()
+        {
+            return this;
+        }
+
+        public Expr Reduce()
+        {
+            return this;
+        }
+
+        public void Jumping(int t, int f)
+        {
+            this.EmitJumps(this.ToString(), t, f);
+        }
+
+        public void EmitJumps(string test, int t, int f)
+        {
+            if(t != 0 && f != 0)
+            {
+                this.Emit("if " + test + " goto L" + t); this.Emit("goto L" + f);
+            }
+            else if(t != 0)
+            {
+                this.Emit("if " + test + " goto L" + t);
+            }
+            else if(f != 0)
+            {
+                this.Emit("iffalse " + test + " goto L" + f);
+            }
+            else
+            {
+                ;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Op.ToString();
+        }
+    }
 }
