8971582b660d4e8b2e35cb2cb97736e0c6f2b11a
moophy <MoophyAlan@gmail.com>
2015-06-22 03:38:37 +1200

    added Constant 	new file:   Dragon/Source/BooleanExpr.cs

---
+++ Dragon/Source/BooleanExpr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragon
+{
+    public class Constant : Expr
+    {
+        public Constant (Token tok, Type type)
+            : base(tok,type)
+        { }
+
+        public Constant(int i)
+            : base(new Num(i), Dragon.Type.Int)
+        { }
+
+        public static readonly Constant
+            True = new Constant(Word.True, Dragon.Type.Bool),
+            False = new Constant(Word.False, Dragon.Type.Bool);
+
+        public override void Jumping(int t, int f)
+        {
+            if (this == Constant.True && t != 0)
+                this.Emit("goto L" + t);
+            else if(this == Constant.False && f != 0)
+                this.Emit("goto L" + f);
+        }
+    }
+}
